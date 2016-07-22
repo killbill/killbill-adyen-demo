acdeeb8404d9e3fc4510ea4828468b964c9c126d
Pierre-Alexandre Meyer <pierre@mouraf.org>
2016-07-22 09:06:53 -0700

    app: directory integration
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- app.rb
+++ app.rb
@@ -30,6 +30,10 @@ def create_kb_account(user, reason, comment, options)
   account.create(user, reason, comment, options)
 end
 
+def get_kb_account(account_id, options)
+  KillBillClient::Model::Account.find_by_id(account_id, false, false, options)
+end
+
 # Credit card auth creating a recurring contract
 def auth(account, encrypted_json, user, reason, comment, options)
   # Adyen contract
@@ -54,14 +58,35 @@ def void(payment, user, reason, comment, options)
   transaction.void(user, reason, comment, options)
 end
 
-# HPP redirect url builder
-def build_form_descriptor(account, invoice, user, reason, comment, options)
+# HPP directory lookup
+def directory_lookup(account, user, reason, comment, options)
   hpp = KillBillClient::Model::HostedPaymentPage.new
   hpp.form_fields = []
 
   {
       :amount => 10,
       :currency => 'USD',
+      :lookupDirectory => true
+  }.each do |key, value|
+    field = KillBillClient::Model::PluginPropertyAttributes.new
+    field.key = key.to_s
+    field.value = value
+    hpp.form_fields << field
+  end
+
+  descriptor = hpp.build_form_descriptor(account.account_id, nil, user, reason, comment, options)
+  (descriptor.form_fields['directory'] || {})['paymentMethods'] || {}
+end
+
+# HPP redirect url builder
+def build_form_descriptor(account, invoice, brand_code, user, reason, comment, options)
+  hpp = KillBillClient::Model::HostedPaymentPage.new
+  hpp.form_fields = []
+
+  {
+      :amount => 10,
+      :currency => 'USD',
+      :brandCode => brand_code,
       :serverUrl => "http://#{settings.bind}:#{settings.port}",
       :resultUrl => "/charge/complete?kbAccountId=#{account.account_id}&kbInvoiceId=#{invoice.invoice_id}"
   }.each do |key, value|
@@ -94,17 +119,17 @@ def pay_invoice(account_id, invoice_id, adyen_params, user, reason, comment, opt
   payment.create(payment, user, reason, comment, options.dup.merge({:pluginProperty => props}))
 end
 
-def create_kb_payment_method(account, encrypted_json, user, reason, comment, options)
-  # Create an empty payment method (Adyen requires a payment to tokenize the card)
+# Create an empty payment method
+def create_kb_payment_method(account, user, reason, comment, options)
   pm = KillBillClient::Model::PaymentMethod.new
   pm.account_id = account.account_id
   pm.plugin_name = 'killbill-adyen'
   pm.plugin_info = {}
   pm.create(true, user, reason, comment, options)
+end
 
-  return if encrypted_json.nil?
-
-  # $1 verification (auth/void)
+def sync_recurring_contract(account, encrypted_json, user, reason, comment, options)
+  # $1 verification (auth/void): Adyen requires a payment to tokenize the card
   payment = auth(account, encrypted_json, user, reason, comment, options)
   void(payment, user, reason, comment, options)
 
@@ -135,22 +160,29 @@ end
 #
 
 get '/' do
+  # Create an account
+  @account = create_kb_account(user, reason, comment, options)
+
+  # Add a payment method
+  create_kb_payment_method(@account, user, reason, comment, options)
+
+  # Look-up available payment methods
+  @directory = directory_lookup(@account, user, reason, comment, options)
+
   erb :index
 end
 
 post '/charge' do
-  # Create an account
-  account = create_kb_account(user, reason, comment, options)
+  account = get_kb_account(params['kb-account-id'], options)
 
   encrypted_cc = params['adyen-encrypted-data']
   is_manual_pay = encrypted_cc.nil?
   if is_manual_pay
     # Tell Kill Bill not to attempt to auto-pay the invoices
     account.set_manual_pay(user, reason, comment, options)
+  else
+    sync_recurring_contract(account, encrypted_cc, user, reason, comment, options)
   end
-
-  # Add a payment method
-  create_kb_payment_method(account, encrypted_cc, user, reason, comment, options)
 
   # Add a subscription
   create_subscription(account, !is_manual_pay, user, reason, comment, options)
@@ -159,7 +191,12 @@ post '/charge' do
   @invoice = account.invoices(true, options).first
 
   if is_manual_pay
-    skin_url = build_form_descriptor(account, @invoice, user, reason, comment, options)
+    brand_code = nil
+    params.each_key do |k|
+      brand_code = k.split('brand-')[1]
+      break unless brand_code.nil?
+    end
+    skin_url = build_form_descriptor(account, @invoice, brand_code, user, reason, comment, options)
     redirect to(skin_url)
   else
     # And the Adyen reference
@@ -205,18 +242,32 @@ __END__
         <span>Sports car, 30 days trial for only $10.00!</span>
       </label>
     </article>
+    <input type="hidden" value="<%= @account.account_id %>" name="kb-account-id" />
     <br/>
     <div><label><span>Card Number</span>&nbsp;<input type="text" size="20" data-encrypted-name="number" /></label></div>
     <div><label><span>Name</span>&nbsp;<input type="text" size="20" data-encrypted-name="holderName" /></label></div>
     <div><label><span>Expiration (MM/YYYY)</span>&nbsp;<input type="text" size="2" maxlength="2" data-encrypted-name="expiryMonth" />/<input type="text" size="4" maxlength="4" data-encrypted-name="expiryYear" /></label></div>
     <div><label><span>CVC</span>&nbsp;<input type="text" size="4" maxlength="4" data-encrypted-name="cvc" /></label></div>
     <input type="hidden" value="<%= Time.now.iso8601 %>" data-encrypted-name="generationtime" />
-    <input type="submit" value="Pay" />
+    <input type="submit" name="cc" value="Pay" />
   </form>
   <br/>
   <form action="/charge" method="post">
-    Or pay via <button name="hpp" value="hpp">HPP</button>
+    <input type="hidden" value="<%= @account.account_id %>" name="kb-account-id" />
+    Or pay via <input type="submit" name="hpp" value="HPP" />
   </form>
+  <br/>
+  Or pay via these payment methods directly:
+  <ul>
+    <% @directory.each do |pm| %>
+      <li>
+        <form action="/charge" method="post">
+          <input type="hidden" value="<%= @account.account_id %>" name="kb-account-id" />
+          <input type="submit" name="brand-<%= pm['brandCode'] %>" value="<%= pm['name'] %>" />
+        </form>
+      </li>
+    <% end %>
+  </ul>
   <script src="https://test.adyen.com/hpp/cse/js/<%= settings.encryption_token %>.shtml"></script>
   <script>
     var form    = document.getElementById('adyen-encrypted-form');
